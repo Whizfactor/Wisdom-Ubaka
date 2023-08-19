9230f262bba2988e3cc0bd111fa45b32eef5cd21
Whizfactor <65239378+Whizfactor@users.noreply.github.com>
2023-08-19 22:32:18 +0100

    3 files modified

--- src/app/components/album/album.component.ts
+++ src/app/components/album/album.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlbumService } from '../../services/album.service';
+import { Album } from '../interface/album'; 
+import { Photo } from '../interface/photo'; 
 
 @Component({
   selector: 'app-album',
@@ -6,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album.component.css']
 })
 export class AlbumComponent implements OnInit {
+albums: Album[];
 
   constructor() { }
 
